1a7c65aec7872ed5a4d33c2b87b00f3ce5beae14
Caleb Crane <calebccrane@gmail.com>
2021-02-05 20:28:05 -0500

    More config changes

--- config.def.h
+++ config.def.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappw     = 10;       /* gap width between windows */
+static const unsigned int gappw     = 7;       /* gap width between windows */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -30,7 +30,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	//{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -61,12 +61,22 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *launchcmd[] = { "rofi", "-show", "run" };
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *lockcmd[]  = { "slock", NULL };
+static const char *restartcmd[]  = { "killall", "dwm", "&" };
+static const char *logoutcmd[]  = { "killall", "Xorg" };
+static const char *rebootcmd[]  = { "shutdown", "-r", "now" };
+static const char *browsercmd[] = { "firefox", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = launchcmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ ControlMask|ShiftMask,        XK_l,      spawn,          {.v = lockcmd } },
+	{ Mod1Mask|ControlMask|ShiftMask, XK_Return, spawn,        {.v = restartcmd } },
+	{ Mod1Mask|ControlMask|ShiftMask, XK_l,     spawn,         {.v = logoutcmd } },
+	{ Mod1Mask|ControlMask|ShiftMask, XK_BackSpace, spawn,     {.v = rebootcmd } },
+	{ MODKEY,                       XK_b,      spawn,          {.v = browsercmd } },
+	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
