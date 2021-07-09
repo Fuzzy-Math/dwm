b8790a5265f4778f3dfd6a4cd2b904e9bbc5153c
Caleb Crane <calebccrane@gmail.com>
2021-07-09 00:22:55 -0400

    Modify config for Fira code font in the status bar

--- config.def.h
+++ config.def.h
@@ -9,8 +9,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 7;       /* vertical padding of bar */
 static const int sidepad            = 7;       /* horizontal padding of bar */
-static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=12";
+static const char *fonts[]          = { "firacode:size=12" };
+static const char dmenufont[]       = "firacode:size=12";
 static const char col_gray1[]       = "#00274c";
 static const char col_gray2[]       = "#131516";
 static const char col_gray3[]       = "#ffcb05";
